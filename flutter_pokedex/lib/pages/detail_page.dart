9ae7b5b145e5a4555676b26d193dcf2c3f650d0b
Murat <104165687+gczmurat@users.noreply.github.com>
2023-09-08 00:51:15 +0300

    detail information

--- flutter_pokedex/lib/pages/detail_page.dart
+++ flutter_pokedex/lib/pages/detail_page.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_pokedex/constants/constants.dart';
 import 'package:flutter_pokedex/constants/ui_helper.dart';
@@ -10,6 +11,7 @@ class DetailPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    String pokeLogoUrl = "images/logo.png";
     return Scaffold(
       backgroundColor: UIHelper.getColorFromType(pokemon.type![0]),
       body: SafeArea(
@@ -40,9 +42,37 @@ class DetailPage extends StatelessWidget {
           ),
           Padding(
             padding: const EdgeInsets.only(left: 30),
-            child: Chip(label: Text(pokemon.type?.join(",")??"", style: Sabitler.pokeYazi2,)),
+            child: Chip(
+                label: Text(
+              pokemon.type?.join(",") ?? "",
+              style: Sabitler.pokeYazi2,
+            )),
           ),
-          PokeInformation(pokemon: pokemon),
+          Stack(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(right: 8,bottom: 50),
+                child: Align(alignment: Alignment.topRight,child: Image.asset(pokeLogoUrl,height: 100,)),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 70),
+                child: Align(
+                    alignment: Alignment.bottomCenter,
+                    child: PokeInformation(pokemon: pokemon)),
+              ),
+              Center(
+                child: Align(
+                  alignment: Alignment.topCenter,
+                  child: CachedNetworkImage(
+                    imageUrl: pokemon.img ?? "",
+                    placeholder: (context, url) =>
+                        const CircularProgressIndicator(color: Colors.red),
+                  ),
+                ),
+              ),
+              
+            ],
+          ),
         ],
       )),
     );
--- flutter_pokedex/lib/widget/poke_information.dart
+++ flutter_pokedex/lib/widget/poke_information.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_pokedex/constants/constants.dart';
 import 'package:flutter_pokedex/model/pokemon_model.dart';
 
 class PokeInformation extends StatelessWidget {
@@ -8,10 +9,125 @@ class PokeInformation extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      height: 483,
       decoration: BoxDecoration(
-        color: Colors.grey.shade300,
+          color: Colors.grey.shade300,
           borderRadius: const BorderRadius.only(
               topLeft: Radius.circular(12), topRight: Radius.circular(12))),
+      child: Column(
+        children: [
+          Container(
+            child: Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 40, left: 10),
+                  child: Text(
+                    "Name :   ${pokemon.name}",
+                    style: Sabitler.pokeYazi1,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const Divider(color: Colors.black),
+          Container(
+            child: Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 10, left: 10),
+                  child: Text(
+                    "Height :   ${pokemon.height}",
+                    style: Sabitler.pokeYazi1,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const Divider(color: Colors.black),
+          Container(
+            child: Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 10, left: 10),
+                  child: Text(
+                    "Weight :   ${pokemon.weight}",
+                    style: Sabitler.pokeYazi1,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const Divider(color: Colors.black),
+          Container(
+            child: Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 10, left: 10),
+                  child: Text(
+                    "Spawn Time :   ${pokemon.spawnTime}",
+                    style: Sabitler.pokeYazi1,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const Divider(color: Colors.black),
+          Container(
+            width: double.infinity,
+            child: Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 10, left: 10),
+                  child: Container(
+                    width: 330,
+                    child: Text(
+                      "Weakness : ${pokemon.weaknesses}",
+                      style: Sabitler.pokeYazi1,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const Divider(color: Colors.black),
+          Container(
+            width: double.infinity,
+            child: Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 10, left: 10),
+                  child: Container(
+                    width: 330,
+                    child: Text(
+                      "Pre Evolution :   ${pokemon.prevEvolution}",
+                      style: Sabitler.pokeYazi1,overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const Divider(color: Colors.black),
+          Container(
+            child: Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 10, left: 10),
+                  child: Container(
+                    width: 330,
+                    child: Text(
+                      "Next Evolution: ${pokemon.nextEvolution}",
+                      style: Sabitler.pokeYazi1,overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
